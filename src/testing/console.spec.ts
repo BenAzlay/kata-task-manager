1fabe9a303ff4aebe161e09e568917a9c473ae51
Benjamin AZOULAY <benjamin.azoulay@protonmail.com>
2021-12-07 16:14:01 +0100

    Add, remove, add test

--- src/testing/console.spec.ts
+++ src/testing/console.spec.ts
@@ -48,3 +48,22 @@ test("Add 2 tasks to task list", (t) => {
     console.readInput('+ Learn Ruby')
     t.deepEqual(console.taskList, ["1 [] Learn Python", "2 [] Learn Ruby"]);
 })
+
+test("Add 2 tasks to task list, remove task 1", (t) => {
+    t.plan(1);
+    const console = new FakeConsole()
+    console.readInput('+ Learn Python')
+    console.readInput('+ Learn Ruby')
+    console.readInput('- 1')
+    t.deepEqual(console.taskList, ["2 [] Learn Ruby"]);
+})
+
+test("Add 2 tasks to task list, remove task 1, add 1", (t) => {
+    t.plan(1);
+    const console = new FakeConsole()
+    console.readInput('+ Learn Python')
+    console.readInput('+ Learn Ruby')
+    console.readInput('- 1')
+    console.readInput('+ Enjoy')
+    t.deepEqual(console.taskList, ["2 [] Learn Ruby", "3 [] Enjoy"]);
+})
