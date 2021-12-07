2f67f694354458dcbe36b8e9bc396283b409909b
Benjamin AZOULAY <benjamin.azoulay@protonmail.com>
2021-12-07 15:06:08 +0100

    Check action for input

--- src/testing/console.spec.ts
+++ src/testing/console.spec.ts
@@ -1,12 +1,27 @@
 import test from 'tape';
 import { FakeConsole } from './fakeConsole';
 
-test("Get operator validity for +", (t) => {
+test("Get action description for '+ Learn Python'", (t) => {
     t.plan(1);
-    t.equal(new FakeConsole().readInput('+ Learn Python'), 'valid');
+    t.equal(new FakeConsole().readInput('+ Learn Python'), "adding Learn Python");
 })
 
-test("Get operator validity for r", (t) => {
+test("Get action description for '- 1'", (t) => {
     t.plan(1);
-    t.equal(new FakeConsole().readInput('r Learn Python'), 'invalid');
+    t.equal(new FakeConsole().readInput('- 1'), "removing task 1");
+})
+
+test("Get action description for 'x 1'", (t) => {
+    t.plan(1);
+    t.equal(new FakeConsole().readInput('x 1'), "checking task 1");
+})
+
+test("Get action description for 'o 1'", (t) => {
+    t.plan(1);
+    t.equal(new FakeConsole().readInput('o 1'), "todoing task 1");
+})
+
+test("Get action description for 'q'", (t) => {
+    t.plan(1);
+    t.equal(new FakeConsole().readInput('q'), "exit");
 })
--- src/testing/fakeConsole.ts
+++ src/testing/fakeConsole.ts
@@ -3,13 +3,22 @@ import { Console } from '../console';
 export class FakeConsole implements Console {
     readInput(input: string) {
         const operator = input[0]
+        const description = input.slice(2, input.length);
+        let index;
         switch(operator) {
             case '+':
+                return `adding ${description}`
             case '-':
+                index = parseInt(description, 10)
+                return `removing task ${index}`
             case 'x':
+                index = parseInt(description, 10)
+                return `checking task ${index}`
             case 'o':
+                index = parseInt(description, 10)
+                return `todoing task ${index}`
             case 'q':
-                return 'valid';
+                return 'exit';
             default:
                 return 'invalid';
         }
